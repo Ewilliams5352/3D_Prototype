da9d0554b4da36cc0c9546910fce98c32bd64bfe
Fiery51 <tmo2@cherrycreekschools.org>
2022-05-17 13:00:34 -0600

    Revert "Deleted all the junk"
    This reverts commit e6b0e8f878be1482cea191186acb41d136e3c821.

---
+++ 3D_Prototype/Assets/Deez.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deez : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I bet this will break everything");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("did you guys also know I can code in python?");
+    }
+}
